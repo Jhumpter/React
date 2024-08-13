491e38818681ee33add4d0387fb26a4f99594e57
Jhumpter <joaopwii@gmail.com>
2024-08-13 12:04:56 -0300

    Trabalhando com estado

--- src/componentes/input/index.js
+++ src/componentes/input/index.js
@@ -1,18 +1,18 @@
 import styled from "styled-components";
 
 const Input = styled.input`
-    border: 1px solid black;
-    background: white;
-    border: 1px solid black;
+    border: 1px solid white;
+    background: transparent;
+    border: 1px solid white;
     padding: 20px 140px;
     border-radius: 50px;
     width: 200px;
-    color: black;
+    color: white;
     font-size: 16px;
     margin-bottom: 10px;
 
     &::placeholder {
-            color: black;
+            color: white;
             font-size: 16px;
     }
 `
--- src/componentes/pesquisa/index.js
+++ src/componentes/pesquisa/index.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import Input from "../input";
+import { useState } from "react";
+//Assim que cria-se um estado no react
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -26,13 +28,15 @@ const Subtitulo = styled.h3`
 O blur é um evento. E, quando lidamos com eventos, podemos manipular os dados com o parâmetro evento, que traz consigo evento.target.value, que representa o elemento escrito no input.*/
 
 function Pesquisa() {
+    /*Para criar um estado cria-se uma constante com colchetes, em que o primeiro elemento é o nome do estado e o segundo é a função que vai 'setar' o estado (que muda o valor dele), utilizando como nome por conveniência setEstado. Essa const vai ser = useState(valor_inicial). Depois é só colocar o set no lugar que é modificado*/ 
+    const [textoDigitado, setTextoDigitado] = useState('')
     return(
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
             <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
-            <Input 
+            <Input
                 placeholder="Encontre a sua próxima leitura"
-                onBlur={evento => console.log(evento.target.value)}
+                onBlur={evento => setTextoDigitado(evento.target.value)}
             />
         </PesquisaContainer>
     )
